3b784b37f08d0719fded20c7f193f34373f929cb
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-18 15:03:34 +0100

    Ajout PIPLINE

--- providers.tf
+++ providers.tf
@@ -9,8 +9,7 @@ terraform {
   }
 
   backend "azurerm" {
-    # Configure the following variables in your environment or tfvars file:
-    # resource_group_name, storage_account_name, container_name, key
+
     resource_group_name  = var.backend_resource_group_name
     storage_account_name = var.backend_storage_account_name
     container_name       = var.backend_container_name
