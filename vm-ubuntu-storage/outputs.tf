7258a997fcc487eebcdd425ea71881e4e950dc4a
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-19 15:24:26 +0100

    feat(PM-3): provision vm-ubuntu-storage — dev

---
+++ vm-ubuntu-storage/outputs.tf
@@ -0,0 +1,9 @@
+output "vm_id" {
+  description = "The ID of the virtual machine."
+  value       = azurerm_virtual_machine.ubuntu_vm.id
+}
+
+output "vm_public_ip" {
+  description = "The public IP address of the virtual machine."
+  value       = azurerm_network_interface.existing.private_ip_address
+}
