e3b6bba497de555d121f57c3aa4bc1fc53b266c7
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-19 15:47:10 +0100

    feat(PM-3): provision vm-ubuntu-storage — dev

---
+++ vm-ubuntu-storage/main.tf
@@ -0,0 +1,49 @@
+locals {
+  tags = var.tags
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.prefix}-vm"
+  location              = var.location
+  resource_group_name   = var.backend_resource_group_name
+  network_interface_ids = [azurerm_network_interface.existing.id]
+  vm_size               = var.vm_size
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}-vm"
+    admin_username = var.admin_username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = var.storage_account_type
+    disk_size_gb      = var.disk_size_gb
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_network_interface" "existing" {
+  name                = var.network_interface
+  location            = var.location
+  resource_group_name = var.backend_resource_group_name
+
+  tags = local.tags
+}
