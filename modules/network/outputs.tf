a9159687e20f14f079cb892ca91229d239c078f3
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-20 19:56:41 +0100

    Delete modules/network directory

--- modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_id" {
-  description = "Virtual network ID created by the module."
-  value       = azurerm_virtual_network.vnet.id
-}
-
-output "agent_subnet_id" {
-  description = "Agent subnet ID created by the module."
-  value       = azurerm_subnet.agent_subnet.id
-}
