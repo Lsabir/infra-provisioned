beff75e0a4479c9f6b28819efb3ed6c8098ed402
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-21 17:45:18 +0100

    feat(PM-6): provision infra-provisioned — dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.tags
 }
+
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "private-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = "vnet-existing"
+  address_prefixes     = ["10.0.2.0/24"]
+  depends_on           = [azurerm_resource_group.rg]
+}
