c96dfe233be6f9f077e8946c0601b5c5e7738b09
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-21 18:30:07 +0100

    feat(PM-6): provision infrastructure-networking — dev

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "resource_group_name" {
+  description = "The name of the resource group."
+  value       = azurerm_resource_group.main.name
+}
+
+output "virtual_network_name" {
+  description = "The name of the virtual network."
+  value       = azurerm_virtual_network.vnet_private.name
+}
+
+output "subnet_name" {
+  description = "The name of the private subnet."
+  value       = azurerm_subnet.subnet_private.name
+}
