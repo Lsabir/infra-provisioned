b9752137d3fff454ce6982b7976e317ccaf01c1a
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-21 21:42:33 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,26 @@
+variable "location" {
+  description = "Azure region."
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name."
+  type        = string
+  default     = "infra-provisioned-rg"
+}
+
 variable "environment" {
-  description = "The environment for the resources."
+  description = "Environment tag."
   type        = string
   default     = "dev"
+}
+
+variable "tags" {
+  description = "Tags applied to all resources."
+  type        = map(string)
+  default = {
+    managed_by  = "terraform"
+    environment = "dev"
+  }
 }
