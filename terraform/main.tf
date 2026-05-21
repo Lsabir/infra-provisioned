ca61d0dadc3399ac1cd10bad299be13ad0a7d86e
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-21 20:58:40 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,23 +3,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.tags
 }
-
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.naming_prefix}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  
-  tags = merge(var.tags, {
-    project = "infra-provisioned"
-    owner   = "devops-team"
-  })
-}
-
-resource "azurerm_subnet" "subnet_private" {
-  name                 = "subnet-private"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
