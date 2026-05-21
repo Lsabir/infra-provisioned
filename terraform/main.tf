b21502ca83e2c0821a984366a8f071f5956526c8
Abde ssamad SABIR <145700789+Lsabir@users.noreply.github.com>
2026-05-21 17:56:17 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,4 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
-resource "azurerm_subnet" "private_subnet" {
-  name                 = "private-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = "vnet-existing"
-  address_prefixes     = ["10.0.2.0/24"]
-  depends_on           = [azurerm_resource_group.rg]
-}
+
